d6c457532de2916167bf761a7e4711ba18d54d4a
baemkkhankll-eng <baemkkhankll@gmail.com>
2026-06-29 00:05:48 +0700

    Update client.js

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Server configuration
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001';
+const SERVER_URL = process.env.SERVER_URL || 'https://remote-control-server-4fdh.onrender.com/';
 
 // Get system information
 const hostname = os.hostname();
